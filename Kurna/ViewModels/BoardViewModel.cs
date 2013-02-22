18fc40828fbe5ee02982d4ba60cbd1d4264d86f4
Dalton Dick <daltondick2010@gmail.com>
2013-02-21 22:50:36 -0600

    Added ReactiveUI and updated teh BoardViewModel

--- Kurna/ViewModels/BoardViewModel.cs
+++ Kurna/ViewModels/BoardViewModel.cs
@@ -4,42 +4,184 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Kurna.Models;
+using ReactiveUI;
 
 namespace Kurna.ViewModels
 {
-    public class BoardViewModel
+    public class BoardViewModel : ReactiveObject
     {
         #region OuterSquare
-        public TileStatus OuterTopLeft = TileStatus.Empty;
-        public TileStatus OuterTopMiddle = TileStatus.Empty;
-        public TileStatus OuterTopRight = TileStatus.Empty;
-        public TileStatus OuterMiddleLeft = TileStatus.Empty;
-        public TileStatus OuterMiddleRight = TileStatus.Empty;
-        public TileStatus OuterBottomLeft = TileStatus.Empty;
-        public TileStatus OuterBottomMiddle = TileStatus.Empty;
-        public TileStatus OuterBottomRight = TileStatus.Empty;
+        private TileStatus _OuterTopLeft = TileStatus.Empty;
+        public TileStatus OuterTopLeft
+        {
+            get{ return _OuterTopLeft; }
+            set { this.RaiseAndSetIfChanged(x => x.OuterTopLeft, value); }
+        }
+
+        private TileStatus _OuterTopMiddle = TileStatus.Empty;
+        public TileStatus OuterTopMiddle
+        {
+            get { return _OuterTopMiddle; }
+            set { this.RaiseAndSetIfChanged(x => x.OuterTopMiddle, value); }
+        }
+
+        private TileStatus _OuterTopRight = TileStatus.Empty;
+        public TileStatus OuterTopRight
+        {
+            get { return _OuterTopRight; }
+            set { this.RaiseAndSetIfChanged(x => x.OuterTopRight, value); }
+        }
+
+        private TileStatus _OuterMiddleLeft = TileStatus.Empty;
+        public TileStatus OuterMiddleLeft
+        {
+            get { return _OuterMiddleLeft; }
+            set { this.RaiseAndSetIfChanged(x => x.OuterMiddleLeft, value); }
+        }
+
+        private TileStatus _OuterMiddleRight = TileStatus.Empty;
+        public TileStatus OuterMiddleRight
+        {
+            get { return _OuterMiddleRight; }
+            set { this.RaiseAndSetIfChanged(x => x.OuterMiddleRight, value); }
+        }
+
+        private TileStatus _OuterBottomLeft = TileStatus.Empty;
+        public TileStatus OuterBottomLeft
+        {
+            get { return _OuterBottomLeft; }
+            set { this.RaiseAndSetIfChanged(x => x.OuterBottomLeft, value); }
+        }
+
+        private TileStatus _OuterBottomMiddle = TileStatus.Empty;
+        public TileStatus OuterBottomMiddle
+        {
+            get { return _OuterBottomMiddle; }
+            set { this.RaiseAndSetIfChanged(x => x.OuterBottomMiddle, value); }
+        }
+
+        private TileStatus _OuterBottomRight = TileStatus.Empty;
+        public TileStatus OuterBottomRight
+        {
+            get { return _OuterBottomRight; }
+            set { this.RaiseAndSetIfChanged(x => x.OuterBottomRight, value); }
+        }
         #endregion
 
         #region MiddleSquare
-        public TileStatus MiddleTopLeft = TileStatus.Empty;
-        public TileStatus MiddleTopMiddle = TileStatus.Empty;
-        public TileStatus MiddleTopRight = TileStatus.Empty;
-        public TileStatus MiddleMiddleLeft = TileStatus.Empty;
-        public TileStatus MiddleMiddleRight = TileStatus.Empty;
-        public TileStatus MiddleBottomLeft = TileStatus.Empty;
-        public TileStatus MiddleBottomMiddle = TileStatus.Empty;
-        public TileStatus MiddleBottomRight = TileStatus.Empty;
+        private TileStatus _MiddleTopLeft = TileStatus.Empty;
+        public TileStatus MiddleTopLeft
+        {
+            get { return _MiddleTopLeft; }
+            set { this.RaiseAndSetIfChanged(x => x.MiddleTopLeft, value); }
+        }
+
+        private TileStatus _MiddleTopMiddle = TileStatus.Empty;
+        public TileStatus MiddleTopMiddle
+        {
+            get { return _MiddleTopMiddle; }
+            set { this.RaiseAndSetIfChanged(x => x.MiddleTopMiddle, value); }
+        }
+
+        private TileStatus _MiddleTopRight = TileStatus.Empty;
+        public TileStatus MiddleTopRight
+        {
+            get { return _MiddleTopRight; }
+            set { this.RaiseAndSetIfChanged(x => x.MiddleTopRight, value); }
+        }
+
+        private TileStatus _MiddleMiddleLeft = TileStatus.Empty;
+        public TileStatus MiddleMiddleLeft
+        {
+            get { return _MiddleMiddleLeft; }
+            set { this.RaiseAndSetIfChanged(x => x.MiddleMiddleLeft, value); }
+        }
+
+        private TileStatus _MiddleMiddleRight = TileStatus.Empty;
+        public TileStatus MiddleMiddleRight
+        {
+            get { return _MiddleMiddleRight; }
+            set { this.RaiseAndSetIfChanged(x => x.MiddleMiddleRight, value); }
+        }
+
+        private TileStatus _MiddleBottomLeft = TileStatus.Empty;
+        public TileStatus MiddleBottomLeft
+        {
+            get { return _MiddleBottomLeft; }
+            set { this.RaiseAndSetIfChanged(x => x.MiddleBottomLeft, value); }
+        }
+
+        private TileStatus _MiddleBottomMiddle = TileStatus.Empty;
+        public TileStatus MiddleBottomMiddle
+        {
+            get { return _MiddleBottomMiddle; }
+            set { this.RaiseAndSetIfChanged(x => x.MiddleBottomMiddle, value); }
+        }
+
+        private TileStatus _MiddleBottomRight = TileStatus.Empty;
+        public TileStatus MiddleBottomRight
+        {
+            get { return _MiddleBottomRight; }
+            set { this.RaiseAndSetIfChanged(x => x.MiddleBottomRight, value); }
+        }
         #endregion
 
         #region InnerSquare
-        public TileStatus InnerTopLeft = TileStatus.Empty;
-        public TileStatus InnerTopMiddle = TileStatus.Empty;
-        public TileStatus InnerTopRight = TileStatus.Empty;
-        public TileStatus InnerMiddleLeft = TileStatus.Empty;
-        public TileStatus InnerMiddleRight = TileStatus.Empty;
-        public TileStatus InnerBottomLeft = TileStatus.Empty;
-        public TileStatus InnerBottomMiddle = TileStatus.Empty;
-        public TileStatus InnerBottomRight = TileStatus.Empty;
+        private TileStatus _InnerTopLeft = TileStatus.Empty;
+        public TileStatus InnerTopLeft
+        {
+            get { return _InnerTopLeft; }
+            set { this.RaiseAndSetIfChanged(x => x.InnerTopLeft, value); }
+        }
+
+        private TileStatus _InnerTopMiddle = TileStatus.Empty;
+        public TileStatus InnerTopMiddle
+        {
+            get { return _InnerTopMiddle; }
+            set { this.RaiseAndSetIfChanged(x => x.InnerTopMiddle, value); }
+        }
+
+        private TileStatus _InnerTopRight = TileStatus.Empty;
+        public TileStatus InnerTopRight
+        {
+            get { return _InnerTopRight; }
+            set { this.RaiseAndSetIfChanged(x => x.InnerTopRight, value); }
+        }
+
+        private TileStatus _InnerMiddleLeft = TileStatus.Empty;
+        public TileStatus InnerMiddleLeft
+        {
+            get { return _InnerMiddleLeft; }
+            set { this.RaiseAndSetIfChanged(x => x.InnerMiddleLeft, value); }
+        }
+
+        private TileStatus _InnerMiddleRight = TileStatus.Empty;
+        public TileStatus InnerMiddleRight
+        {
+            get { return _InnerMiddleRight; }
+            set { this.RaiseAndSetIfChanged(x => x.InnerMiddleRight, value); }
+        }
+
+        private TileStatus _InnerBottomLeft = TileStatus.Empty;
+        public TileStatus InnerBottomLeft
+        {
+            get { return _InnerBottomLeft; }
+            set { this.RaiseAndSetIfChanged(x => x.InnerBottomLeft, value); }
+        }
+
+        private TileStatus _InnerBottomMiddle = TileStatus.Empty;
+        public TileStatus InnerBottomMiddle
+        {
+            get { return _InnerBottomMiddle; }
+            set { this.RaiseAndSetIfChanged(x => x.InnerBottomMiddle, value); }
+        }
+
+        private TileStatus _InnerBottomRight = TileStatus.Empty;
+        public TileStatus InnerBottomRight
+        {
+            get { return _InnerBottomRight; }
+            set { this.RaiseAndSetIfChanged(x => x.InnerBottomRight, value); }
+        }
         #endregion
     }
 }
