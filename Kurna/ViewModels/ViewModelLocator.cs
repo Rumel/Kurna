d053f09864b8f698b80e7a08945bc79a869adc30
Dalton Dick <daltondick2010@gmail.com>
2013-03-10 22:28:37 -0500

    Added ViewModelLocator

---
+++ Kurna/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kurna.ViewModels
+{
+    public static class ViewModelLocator
+    {
+        private BoardViewModel _boardViewModel = null;
+        public BoardViewModel BoardViewModel
+        {
+            get
+            {
+                if (_boardViewModel == null)
+                {
+                    _boardViewModel = new BoardViewModel();
+                }
+                return _boardViewModel;
+            }
+            set
+            {
+                _boardViewModel = value;
+            }
+        }
+    }
+}
