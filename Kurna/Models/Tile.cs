ee53c8132310faf487d9dd5589dd79b859dba27a
Caleb Jares <cable729@gmail.com>
2013-03-13 13:37:24 -0500

    Gameplay phase 1 done

--- Kurna/Models/Tile.cs
+++ Kurna/Models/Tile.cs
@@ -10,7 +10,14 @@ namespace Kurna.Models
         public static readonly Brush P1Color = Brushes.Aqua;
         public static readonly Brush P2Color = Brushes.BlueViolet;
         private Brush fillColor;
+        private int strokeThickness;
         private TileStatus tileStatus;
+
+        public Tile()
+        {
+            Status = TileStatus.Unoccupied;
+            StrokeThickness = 0;
+        }
 
         public TileStatus Status
         {
@@ -22,10 +29,25 @@ namespace Kurna.Models
             get { return fillColor; }
             set { this.RaiseAndSetIfChanged(ref fillColor, value); }
         }
-        public string Name { get; set; }
+        public int StrokeThickness
+        {
+            get { return strokeThickness; }
+            set { this.RaiseAndSetIfChanged(ref strokeThickness, value); }
+        }
+        public string TileName { get; set; }
         public int Row { get; set; }
         public int Column { get; set; }
         public IEnumerable<Tile> AdjacentTiles { get; set; }
+
+        public void ShowIsAvailable()
+        {
+            StrokeThickness = 5;
+        }
+
+        public void ShowIsUnavailable()
+        {
+            StrokeThickness = 0;
+        }
 
         private void OnTileStatusChanged(TileStatus value)
         {
@@ -36,11 +58,6 @@ namespace Kurna.Models
             if (value == TileStatus.P2)
                 FillColor = P2Color;
             this.RaiseAndSetIfChanged(ref tileStatus, value);
-        }
-
-        public Tile()
-        {
-            Status = TileStatus.Unoccupied;
         }
     }
 }
--- Kurna/Views/Board.xaml.cs
+++ Kurna/Views/Board.xaml.cs
@@ -1,10 +1,15 @@
-﻿using System.Windows.Controls;
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Shapes;
+using Kurna.Models;
+using Kurna.ViewModels;
 
 namespace Kurna.Views
 {
     /// <summary>
-    ///     Interaction logic for Board.xaml
+    /// Interaction logic for Board.xaml
     /// </summary>
     public partial class Board : UserControl
     {
@@ -13,7 +18,99 @@ namespace Kurna.Views
             InitializeComponent();
         }
 
-        private void UIElement_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        private void Ellipse_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var ellipse = sender as Ellipse;
+            if (ellipse == null) return;
+
+            var game = ViewModelLocator.GameViewModel.Game;
+            var players = ViewModelLocator.PlayerViewModel;
+            var tile = game.Tiles.FirstOrDefault(t => ellipse.Tag as string == t.TileName);
+            if (game.State == GameState.Placing)
+            {
+                if (tile.Status != TileStatus.Unoccupied)
+                {
+                    // can't place on top of another piece 
+                }
+                else if (players.PlayerOne.IsPlayersTurn)
+                {
+                    if (players.PlayerOne.PiecesLeft == 0)
+                    {
+                        game.State = GameState.Moving;
+                    }
+                    else
+                    {
+                        tile.Status = TileStatus.P1;
+                        players.PlayerOne.PiecesLeft--;
+                        players.SwitchTurns();
+                    }
+                }
+                else if (players.PlayerTwo.IsPlayersTurn)
+                {
+                    if (players.PlayerTwo.PiecesLeft == 0)
+                    {
+                        game.State = GameState.Moving;
+                    }
+                    else
+                    {
+                        tile.Status = TileStatus.P2;
+                        players.PlayerTwo.PiecesLeft--;
+                        players.SwitchTurns();
+                    }
+                }
+            }
+
+            //var tiles = ellipse.Tag as IEnumerable<Tile>;
+            //if (tiles != null)
+            //    foreach (var tile in tiles)
+            //    {
+            //        tile.ShowIsAvailable();
+            //    }
+        }
+
+        private void ShowUnavailable(object sender, MouseButtonEventArgs e)
+        {
+            //var ellipse = sender as Ellipse;
+
+            //var tiles = ellipse.Tag as IEnumerable<Tile>;
+            //if (tiles != null)
+            //    foreach (var tile in tiles)
+            //    {
+            //        tile.ShowIsUnavailable();
+            //    }
+        }
+
+        private void ShowUnavailable2(object sender, MouseEventArgs e)
+        {
+            //ShowUnavailable(sender, null);
+        }
+
+        private void ellipse_MouseMove(object sender, MouseEventArgs e)
+        {
+            //var ellipse = sender as Ellipse;
+            //if (ellipse != null && e.LeftButton == MouseButtonState.Pressed)
+            //{
+            //    DragDrop.DoDragDrop(ellipse, ellipse.Fill.ToString(), DragDropEffects.Move);
+            //}
+        }
+
+        private void ellipse_GiveFeedback(object sender, GiveFeedbackEventArgs e)
+        {
+        }
+
+        private void ellipse_DragEnter(object sender, DragEventArgs e)
+        {
+        }
+
+        private void ellipse_DragLeave(object sender, DragEventArgs e)
+        {
+        }
+
+        private void ellipse_DragOver(object sender, DragEventArgs e)
+        {
+        }
+
+        private void ellipse_Drop(object sender, DragEventArgs e)
         {
         }
     }
