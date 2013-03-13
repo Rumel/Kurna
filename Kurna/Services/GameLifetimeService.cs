cc94a44f5ff971efdff3544c07b92de9a891f1e1
Caleb Jares <cable729@gmail.com>
2013-03-13 00:59:36 -0500

    test

---
+++ Kurna/Services/GameLifetimeService.cs
@@ -0,0 +1,15 @@
+﻿using Kurna.Models;
+using ReactiveUI;
+
+namespace Kurna.Services
+{
+    public class GameLifetimeService : ReactiveObject
+    {
+        private Game currentGame;
+        public Game CurrentGame
+        {
+            get { return currentGame; }
+            set { this.RaiseAndSetIfChanged(ref currentGame, value); }
+        }
+    }
+}
---
+++ Kurna/ViewModels/GameViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Kurna.Models;
+using ReactiveUI;
+
+namespace Kurna.ViewModels
+{
+    public class GameViewModel : ReactiveObject
+    {
+        private Game game;
+        public Game Game
+        {
+            get { return game; }
+            set { this.RaiseAndSetIfChanged(ref game, value); }
+        }
+    }
+}
